6049e3e23dfd7cc5d37b324cc0d8cfc4902ee5f1
Khanh Nguyen <duykhanhlb4999@gmail.com>
2026-05-02 20:47:52 +0700

    Initial Commit

---
+++ VoiceExpenseTracker/VoiceExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceExpenseTrackerApp.swift
+//  VoiceExpenseTracker
+//
+//  Created by KhanhNguyen on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
